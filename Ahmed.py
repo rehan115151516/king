1b2bf5a940ab03910706090155a8611f22e8c0fb
rehan115151516 <64297272+rehan115151516@users.noreply.github.com>
2020-05-07 06:53:06 +0500

    Update Ahmed.py

--- Ahmed.py
+++ Ahmed.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python2
 #coding=utf-8
-#The Credit For This Code Goes To Raza
+#The Credit For This Code Goes To Rehan
 #If You Wanna Take Credits For This Code, Please Look Yourself Again...
 #Reserved2020
 
